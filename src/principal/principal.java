0abcc0909be2e4ba864a7eb24b438c012e7f8474
Rodolfo Nemes <tecnico.rodolfoo@gmail.com>
2022-09-04 22:24:14 -0300

    FileWriter e BufferedWriter

--- src/principal/principal.java
+++ src/principal/principal.java
@@ -1,23 +1,22 @@
 package principal;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class principal {
 
 	public static void main(String[] args) {
-
-		String path = "D:\\usuario\\rodolfo\\Desktop\\eclipse\\workspace\\manipulacaoArquivos\\in.txt";
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			String line = br.readLine();
-			while (line != null) {
-			System.out.println(line);
-			line = br.readLine();
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+		String path = "D:\\usuario\\rodolfo\\Desktop\\eclipse\\workspace\\manipulacaoArquivos\\out.txt";
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
-			} catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-			}
+		}
 
 	}
 }
