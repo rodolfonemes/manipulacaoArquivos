f859bcf71de557b9acf45188e127d52212cb8f87
Rodolfo Nemes <tecnico.rodolfoo@gmail.com>
2022-09-04 22:32:03 -0300

    FileWriter e BufferedWriter v2

--- src/principal/principal.java
+++ src/principal/principal.java
@@ -9,7 +9,11 @@ public class principal {
 	public static void main(String[] args) {
 		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
 		String path = "D:\\usuario\\rodolfo\\Desktop\\eclipse\\workspace\\manipulacaoArquivos\\out.txt";
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			
+			bw.write("----------------------------------");
+			bw.newLine();
+			
 			for (String line : lines) {
 				bw.write(line);
 				bw.newLine();
