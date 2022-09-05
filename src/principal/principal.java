9ebdefa9ec94602a7daf7a9f4f47f3a1619d8a00
Rodolfo Nemes <tecnico.rodolfoo@gmail.com>
2022-09-04 22:46:54 -0300

    Informações do caminho do arquivo

--- src/principal/principal.java
+++ src/principal/principal.java
@@ -10,18 +10,9 @@ public class principal {
 		System.out.println("Enter a folder path: ");
 		String strPath = sc.nextLine();
 		File path = new File(strPath);
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS:");
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("FILES:");
-		for (File file : files) {
-			System.out.println(file);
-		}
-		boolean success = new File(strPath + "\\subdir").mkdir();
-		System.out.println("Directory created successfully: " + success);
+		System.out.println("getPath: " + path.getPath());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getName: " + path.getName());
 		sc.close();
 
 	}
